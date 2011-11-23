2c15fa858e8e243711770f74356387872a257400
CazH <cazh88@gmail.com>
2011-11-23 22:23:29 +0100

    Build command parser

--- chiller/firmware/chillerctrl.c
+++ chiller/firmware/chillerctrl.c
@@ -11,6 +11,17 @@
 #include "uart.c"
 
 char uart_buf[40];
+char power;
+int store_max;
+int store_min;
+int buffer_set;
+int post_run;
+int storage;
+int buffer;
+int cooling_pwm;
+char compressor;
+char fan;
+
 
 void ParseCMD();
 
@@ -43,7 +54,7 @@ int main(void)
 
 			if(newchar == 0x0d)
 			{
-				//Parse CMD
+				ParseCMD();
 				memset(uart_buf, 0, sizeof(uart_buf));
 				buf_p = &uart_buf[0];
 			} else {
@@ -65,4 +76,114 @@ int main(void)
 
 	}
 
+}
+
+
+void ParseCMD()
+{
+	char option[5];
+	int number[4];
+
+	if(sscanf(uart_buf,"power=%s",option))
+	{
+		if(option[1] == 'n')
+		{
+			power = 1;
+			option[4] = 'o';
+		}
+		else if(option[1] == 'f')
+		{
+			power = 0;
+			option[4] = 'o';
+		}
+		else
+		{
+			option[4] = 'n';
+		}
+
+	}
+	else if(sscanf(uart_buf,"store-max=%d",number))
+	{
+		if(number[0])
+		{
+			store_max = number[0];
+			option[4] = 'o';
+		}
+		else
+		{
+			printf("%d\n",number[0]);
+			option[4] = 'n';
+		}
+	}
+        else if(sscanf(uart_buf,"store-min=%d",number))
+        {
+                if(option[0])
+                {
+                        store_min = number[0];
+                        option[4] = 'o';
+                }
+                else
+                {
+                        option[4] = 'n';
+                }
+        }
+        else if(sscanf(uart_buf,"buffer-set=%d",number))
+        {
+                if(option[0])
+                {
+                        buffer_set = number[0];
+                        option[4] = 'o';
+                }
+                else
+                {
+                        option[4] = 'n';
+                }
+        }
+        else if(sscanf(uart_buf,"post-run=%d",number))
+        {
+                if(option[0])
+                {
+                        post_run = number[0];
+                        option[4] = 'o';
+                }
+                else
+                {
+                        option[4] = 'n';
+                }
+        }
+	else if(sscanf(uart_buf,"status%s",option))
+	{
+		option[4] = 's';
+
+	}
+	else
+	{
+		printf("ERROR: Command not found\n");
+	}
+
+	if(option[4] == 'o')
+	{
+		printf("ok\n");
+	}
+	else if(option[4] == 'n')
+	{
+		printf("nok\n");
+	}
+	else if(option[4] == 's')
+	{
+		printf("ok\n");
+		if(power){ printf("power: on\n"); } else { printf("power: off\n"); }
+                printf("store-max: %d\n",store_max);
+                printf("store-min: %d\n",store_min);
+                printf("buffer-set: %d\n",buffer_set);
+                printf("post-run: %d\n",post_run);
+                printf("storage: %d\n",storage);
+                printf("buffer: %d\n",buffer);
+                printf("cooling-pwm: %d\n",cooling_pwm);
+		if(compressor){ printf("compressor: on\n"); } else { printf("compressor: off\n"); }
+                if(fan){ printf("fan: on\n"); } else { printf("fan: off\n"); }
+
+	}
+
+	memset(option,0,sizeof(option));
 }
