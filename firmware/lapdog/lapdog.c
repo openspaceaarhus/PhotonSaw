d5841d2a86f45aa55fc9e21fca9f83e177e7b580
Flemming Frandsen <dren.dk@gmail.com>
2012-02-17 23:22:51 +0100

    Added dummy watchdog that just toggles its LEDS, outputs and sends OK to allow testing without coding a proper watchdog

--- firmware/lapdog/lapdog.c
+++ firmware/lapdog/lapdog.c
@@ -24,8 +24,10 @@
 // We don't really care about unhandled interrupts.
 EMPTY_INTERRUPT(__vector_default)
 
+
 // A macro and function to store string constants in flash and only copy them to
 // RAM when needed, note the limit on string length.
+
 char stringBuffer[80];
 
 const char *getString(PGM_P src) {
@@ -36,48 +38,68 @@ const char *getString(PGM_P src) {
 
 #define PROGSTR(s) getString(PSTR(s))
 
+
 void led1(char on) {
   if (on) {
-    PORTC |= 1<<PB6;   
+    PORTB |= _BV(PB6);   
   } else {
-    PORTC &=~ 1<<PB6;   
+    PORTB &=~ _BV(PB6);   
   }
 }
 
 void led2(char on) {
   if (on) {
-    PORTC |= 1<<PB7;   
+    PORTB |= _BV(PB7);   
   } else {
-    PORTC &=~ 1<<PB7;   
+    PORTB &=~ _BV(PB7);   
   }
 }
 
+void stop() {
+  PORTB &=~ _BV(PB2);
+  PORTC &=~ _BV(PC1);
+}
+
+void run() {
+  PORTB |= _BV(PB2);
+  PORTC |= _BV(PC1);
+}
+
+
 int main(void) {
+  DDRB  |= _BV(PB6) | _BV(PB7);  // LED outputs
+  PORTB |= _BV(PB7) | _BV(PB6);
+
+  DDRB  |= _BV(PB2);  // Enable Motors output
+  DDRC  |= _BV(PC1);  // Enable LASER outout 
+  
   wdt_enable(WDTO_4S);
   
-  // The fuses set the clock prescaler to divide by 8, so we need to 
-  // turn up the clock speed:
-  CLKPR = 1<<CLKPCE; CLKPR = CLOCK_PRESCALER;
-
-  DDRB |= 1<<PB6 | 1<<PB6;  // LED outputs
-  led1(1);
-
   uart_init();
   FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
   stdout = stdin = &uart_str;
   fprintf(stdout, PROGSTR("#Power up!\n"));
   
-  unsigned int frame = 0;
+  led1(0);
+  led2(0);
+
+  char frame = 0;
   while(1) {
-    fprintf(stdout, PROGSTR("frame=%u\n"), frame);
+    if (!(frame & 15)) {
+      fprintf(stdout, PROGSTR("OK\n"));
+    }
 
     if (frame & 8) {
-      led1(frame & (1|4));
+      led1(!(frame & 1));
+      run();
+  
     } else {
-      led2(frame & (1|4));
+      led2(!(frame & 1));
+      stop();
     }
     
-    sleepMs(100);
+    sleepMs(50);
     wdt_reset();
+    frame++;
   }	
 }
