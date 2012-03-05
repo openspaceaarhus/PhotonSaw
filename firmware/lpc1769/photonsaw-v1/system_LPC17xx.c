f074b5a9ad29891286f7504935d59e761bec061d
Flemming Frandsen <dren.dk@gmail.com>
2012-03-05 16:20:01 +0100

    Make slightly speedier reads from flash, because the datasheet says it's ok

--- firmware/lpc1769/photonsaw-v1/system_LPC17xx.c
+++ firmware/lpc1769/photonsaw-v1/system_LPC17xx.c
@@ -314,7 +314,7 @@
 // </e>
 */
 #define FLASH_SETUP           1
-#define FLASHCFG_Val          0x0000503A
+#define FLASHCFG_Val          0x0000403A
 
 /*
 //-------- <<< end of configuration section >>> ------------------------------
