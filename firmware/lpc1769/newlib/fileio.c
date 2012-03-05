e4045365b557fcc408853f25d2419fe0e76f7af9
Flemming Frandsen <dren.dk@gmail.com>
2012-03-05 21:37:24 +0100

    Starting to think about how to piece together the VFS

--- firmware/lpc1769/newlib/fileio.c
+++ firmware/lpc1769/newlib/fileio.c
@@ -2,6 +2,36 @@
 #include "board.h"
 #include "uarts.h"
 
+typedef struct {
+  char *NAME;
+  int FD;
+  
+
+} File;
+
+int _open(const char *name, int flags, int mode) {
+
+  if (!strcmp(name, "/dev/stdin")) {
+    return 0;
+
+  } else if (!strcmp(name, "/dev/stdout")) {
+    return 1;
+    
+  } else if (!strcmp(name, "/dev/stderr")) {
+    return 2;
+
+  } else if (!strcmp(name, "/dev/watchdog")) {
+    return 3;
+
+  } else if (!strcmp(name, "/dev/chiller")) {
+    return 4;
+
+  } 
+
+
+  return -1;
+}
+
 int _close(int file) { return -1; }
 
 int _fstat(int file, struct stat *st) {
@@ -13,7 +43,6 @@ int _isatty(int file) { return 1; }
 
 int _lseek(int file, int ptr, int dir) { return 0; }
 
-int _open(const char *name, int flags, int mode) { return -1; }
 
 int _read(int file, char *ptr, int len) {
   if(len == 0) return 0;
