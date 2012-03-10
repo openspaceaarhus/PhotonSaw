99666e5ba30aad88e5c24280fcee89c38c8d31a3
CazH <cazh88@gmail.com>
2012-03-10 19:50:36 +0100

    Added default values and ADC read function

--- chiller/firmware/chillerctrl.c
+++ chiller/firmware/chillerctrl.c
@@ -12,10 +12,10 @@
 
 char uart_buf[40];
 char power;
-int store_max;
-int store_min;
-int buffer_set;
-int post_run;
+int store_max = 18;
+int store_min = 2;
+int buffer_set = 25;
+int post_run = 30;
 int storage;
 int buffer;
 int cooling_pwm;
@@ -24,6 +24,19 @@ char fan;
 
 
 void ParseCMD();
+void ReadRegister();
+
+uint16_t ReadADC(uint8_t ADCchannel)
+{
+        //select ADC channel with safety mask
+        ADMUX = (ADMUX & 0xF0) | (ADCchannel & 0x0F);
+        //single conversion mode
+        ADCSRA |= (1<<ADSC);
+        // wait until ADC conversion is complete
+        while( ADCSRA & (1<<ADSC) );
+        return ADC;
+}
+
 
 int main(void)
 {
@@ -40,6 +53,13 @@ int main(void)
 	char newchar;
 
 	buf_p = &uart_buf[0];
+
+//	Setup ADC
+
+	 // Select Vref=AVcc
+	 ADMUX |= (1<<REFS0);
+	 //set prescaller to 128 and enable ADC
+	 ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
 
 //	Main loop
 
