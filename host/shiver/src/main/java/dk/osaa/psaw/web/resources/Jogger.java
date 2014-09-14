d6b67d0481556c87a7f8552a64082835bfc3f66f
Flemming Frandsen <dren.dk@gmail.com>
2014-09-14 18:40:40 +0200

    Added the annotations needed to make swagger work

--- host/shiver/src/main/java/dk/osaa/psaw/web/resources/Jogger.java
+++ host/shiver/src/main/java/dk/osaa/psaw/web/resources/Jogger.java
@@ -10,21 +10,24 @@ import lombok.AllArgsConstructor;
 
 import com.codahale.metrics.annotation.Timed;
 import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
 
 import dk.osaa.psaw.core.PhotonSaw;
 import dk.osaa.psaw.web.api.JogSpeed;
 import dk.osaa.psaw.web.api.JogStatus;
 
-@Api(value = "/jogger", description = "Sets the jogging speed in all axes")
-@AllArgsConstructor
 @Path("/jogger")
+@Api(value = "/api/jogger", description = "Jogging operations")
 @Produces(MediaType.APPLICATION_JSON)
+@AllArgsConstructor
 public class Jogger {
 	private final PhotonSaw psaw;
 	
+	@ApiOperation(value = "Starts or maintains the specified speed vector, the machine will time out after 500 ms so the jog command must be repeated.")
 	@POST
 	@Timed
-	public JogStatus set(@Valid JogSpeed speed) {
+	public JogStatus set(@ApiParam(value = "The speed vector of the machine in mm/s", required = true) @Valid JogSpeed speed) {
 		JogStatus result = new JogStatus("Test");
 		psaw.setJogSpeed(speed.toMoveVector());
 		return result;
