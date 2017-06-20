6e84039c228fe2e3c401fd4ad9c0cf3861e27adc
Henrik Brix Andersen <henrik@brixandersen.dk>
2017-06-20 19:55:31 +0200

    Make shiver read and understand SVG width, height and viewBox attributes - and use them for scaling the resulting physical object.

--- host/shiver/src/main/java/dk/osaa/psaw/job/SvgLoader.java
+++ host/shiver/src/main/java/dk/osaa/psaw/job/SvgLoader.java
@@ -1,6 +1,7 @@
 package dk.osaa.psaw.job;
 
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -14,6 +15,7 @@ import com.kitfox.svg.SVGDiagram;
 import com.kitfox.svg.SVGException;
 import com.kitfox.svg.SVGUniverse;
 import com.kitfox.svg.animation.AnimationElement;
+import com.kitfox.svg.xml.NumberWithUnits;
 import com.kitfox.svg.xml.StyleAttribute;
 
 import dk.osaa.psaw.config.PhotonSawMachineConfig;
@@ -36,62 +38,96 @@ public class SvgLoader {
 		URI svgURI = su.loadSVG(svgStream, name);
 		SVGDiagram diagram = su.getDiagram(svgURI);
 		val e = diagram.getRoot();
-		
-		/* Try to guess the user units to real world conversion factor, because the nitwits
-		 * at w3c didn't bother to specify a default pixel size or an easy way to specify it.
-		 * 
-		 * inkscape seems to just work in 90 DPI implicitly, without stating that anywhere, let alone in an
-		 * attribute in the xml, so let's hope they never change their minds.
-		 * 
-		 * Everyone else seems to use either 96 DPI or 72 DPI if they are in a postscript state of mind.
-		 * 
-		 * Fuck!
-		 * 
-		 * Why? Why, would you do something like that, were you evil or just stupid?
-		 */
-		
-		double pixelsSizeX = 25.4/96; // Default to 96 DPI, perhaps we should default to 2540 DPI, just to make uncertainty very obvious.
-		double pixelsSizeY = pixelsSizeX;
-		
+
+		// SVG DPI, default to 96 DPI as recommended by the SVG Working Group (and used in e.g. Inkscape 0.92 and later)
+		double dpiX = 96;
+		double dpiY = 96;
+
+		// Page size and viewbox attributes
+		StyleAttribute widthAttr = new StyleAttribute("width");
+		StyleAttribute heightAttr = new StyleAttribute("height");
+		StyleAttribute viewBoxAttr = new StyleAttribute("viewBox");
+
 		if (forcedDPI > 0) {
-			pixelsSizeX = pixelsSizeY = 25.4/forcedDPI;
-			log.info("Explicit DPI set by SVG loader: "+forcedDPI);			
-			
+			dpiX = dpiY = forcedDPI;
+			log.warning("Explicit default DPI set by SVG loader: "+dpiX+"x"+dpiY);
 		} else if (e.hasAttribute("photonsaw-dpi", AnimationElement.AT_XML)) {
 			StyleAttribute dpiAttr = new StyleAttribute("photonsaw-dpi");
 			e.getPres(dpiAttr);
-			pixelsSizeX = pixelsSizeY = 25.4/dpiAttr.getDoubleValue();
-			log.info("Explicit DPI set by photonsaw-dpi attribute: "+dpiAttr.getDoubleValue());
-			
+			dpiX = dpiY = dpiAttr.getDoubleValue();
+			log.warning("Explicit default DPI set by photonsaw-dpi attribute: "+dpiX+"x"+dpiY);
 		} else if (e.hasAttribute("inkscape:version", AnimationElement.AT_XML)) {
 			StyleAttribute versionAttr = new StyleAttribute("inkscape:version");
 			e.getPres(versionAttr);
 			String inkscapeVersion = versionAttr.getStringValue();
 
-			int dpi = 96;
-			
 			Matcher majorMinor = MAJOR_MINOR_VERSION.matcher(inkscapeVersion);
 			if (majorMinor.find()) {
 				long major = Long.parseLong(majorMinor.group(1));
 				long minor = Long.parseLong(majorMinor.group(2));
 				log.fine("Parsed inkscape version: "+major+"."+minor);
 				if (major == 0 && minor <= 91) {
-					dpi = 90;
+					dpiX = dpiY = 90;
 				}
 			}
-			
-			pixelsSizeX = pixelsSizeY = 25.4/dpi;
-			log.info("Inkscape version "+inkscapeVersion+" svg detected: "+name+" assuming "+dpi+" DPI for conversion to mm");
-
-		} else {
-			log.warning("Unable to guess the pixel size used in the svg file "+name+" guessing 96 DPI");
+			log.info("Inkscape version "+inkscapeVersion+" svg detected: "+name+" assuming "+dpiX+"x"+dpiY+" default DPI");
 		}
-		
-		// Create the affine transform that is used to convert from svg pixels to mm
-		AffineTransform pixel2mm = AffineTransform.getScaleInstance(pixelsSizeX, pixelsSizeY);
-				
-		JobNodeGroup res = new JobNodeGroup(job.getNodeId(name), pixel2mm);
+
+		if (e.getPres(widthAttr) && e.getPres(heightAttr) && e.getPres(viewBoxAttr)) {
+			// Calculate DPI from page size and view box
+			NumberWithUnits width = widthAttr.getNumberWithUnits();
+			NumberWithUnits height = heightAttr.getNumberWithUnits();
+			float[] coords = viewBoxAttr.getFloatList();
+			Rectangle2D.Float viewBox = new Rectangle2D.Float(coords[0], coords[1], coords[2], coords[3]);
+
+			// Calculate DPI from page size and viewbox
+			if (width.getUnits() == NumberWithUnits.UT_IN) {
+				dpiX = viewBox.getWidth()/width.getValue();
+			} else if (width.getUnits() == NumberWithUnits.UT_MM) {
+				dpiX = viewBox.getWidth()/width.getValue()*25.4;
+			} else if (width.getUnits() == NumberWithUnits.UT_CM) {
+				dpiX = viewBox.getWidth()/width.getValue()*2.54;
+			} else if (width.getUnits() == NumberWithUnits.UT_PX || width.getUnits() == NumberWithUnits.UT_UNITLESS) {
+				dpiX = viewBox.getWidth()/(width.getValue()/dpiX);
+			} else {
+				log.warning("Unsupported unit "+NumberWithUnits.unitsAsString(width.getUnits())+"on SVG width attribute, ignoring SVG width scale factor");
+			}
+
+			if (height.getUnits() == NumberWithUnits.UT_IN) {
+				dpiY = viewBox.getHeight()/height.getValue();
+			} else if (height.getUnits() == NumberWithUnits.UT_MM) {
+				dpiY = viewBox.getHeight()/height.getValue()*25.4;
+			} else if (height.getUnits() == NumberWithUnits.UT_CM) {
+				dpiY = viewBox.getHeight()/height.getValue()*2.54;
+			} else if (height.getUnits() == NumberWithUnits.UT_PX || height.getUnits() == NumberWithUnits.UT_UNITLESS) {
+				dpiY = viewBox.getHeight()/(width.getValue()/dpiY);
+			} else {
+				log.warning("Unsupported unit "+NumberWithUnits.unitsAsString(height.getUnits())+"on SVG height attribute, ignoring SVG height scale factor");
+			}
+		} else {
+			log.warning("No width/height/viewBox attributes in SVG file "+name+", assuming "+dpiX+"x"+dpiY+" DPI");
+		}
+
+		// Remove width, height and viewBox attributes - if present - as these cause svgSalamander to do its own scaling of the renderered SVG
+		if (e.getPres(widthAttr)) {
+		    e.removeAttribute(widthAttr.getName(), AnimationElement.AT_XML);
+		}
+		if (e.getPres(heightAttr)) {
+		    e.removeAttribute(heightAttr.getName(), AnimationElement.AT_XML);
+		}
+		if (e.getPres(viewBoxAttr)) {
+		    e.removeAttribute(viewBoxAttr.getName(), AnimationElement.AT_XML);
+		}
+		su.updateTime();
+
+		log.info("Scaling SVG file "+name+" from "+dpiX+"x"+dpiY+" DPI to 96x96 DPI");
+
+		// Create the affine transform that is used to convert from SVG DPI to 96 DPI
+		AffineTransform scaleTo96dpi = AffineTransform.getScaleInstance(25.4/dpiX, 25.4/dpiY);
+
+		JobNodeGroup res = new JobNodeGroup(job.getNodeId(name), scaleTo96dpi);
 		Graphics2DJobNodeGroup g2d = new Graphics2DJobNodeGroup(job, res);
+		// TODO: different resolution in x/y?
 		g2d.setResolution(25.4/cfg.getMmPerStep().getAxis(0));
 		diagram.setIgnoringClipHeuristic(true);
 		
